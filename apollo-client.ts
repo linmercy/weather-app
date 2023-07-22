a6b4652207a9816fa2b9fb9861e509972fea4b03
mercylin muthoni <linmercymuthoni@gmail.com>
2023-07-22 22:35:38 +0300

    implementing apollo client for graphql

---
+++ apollo-client.ts
@@ -0,0 +1,17 @@
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+
+// let client: ApolloClient<any> | null =null;
+
+export const getClient = () => {
+    const client = new ApolloClient({
+        uri: process.env.API_URL,
+        cache: new InMemoryCache(),
+        headers: {
+        Authorization: `apikey ${process.env.NEXT_STEPZEN_API_KEY}`,
+        },
+    });
+    
+    return client;
+};
+
+
