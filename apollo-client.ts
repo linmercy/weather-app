d524fbeae1340e678b98dd8b704e7833fa4617b4
mercylin muthoni <linmercymuthoni@gmail.com>
2023-07-24 21:33:59 +0300

    updates

--- apollo-client.ts
+++ apollo-client.ts
@@ -1,6 +1,5 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
-// let client: ApolloClient<any> | null =null;
 
 export const getClient = () => {
     const client = new ApolloClient({
